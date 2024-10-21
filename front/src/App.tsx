700a3bf5a1f1dd261c0b4a52b4cdb7fb65eb88ca
apeltop <sunshine@ptokos.com>
2024-10-21 12:15:45 +0900

    feat: front 채팅 템플릿 setup 하였다.

---
+++ front/src/App.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { CssVarsProvider } from '@mui/joy/styles';
+import CssBaseline from '@mui/joy/CssBaseline';
+import Box from '@mui/joy/Box';
+import Sidebar from './messages/components/Sidebar';
+import Header from './messages/components/Header';
+import MyMessages from './messages/components/MyMessages';
+
+export default function JoyMessagesTemplate() {
+  return (
+    <CssVarsProvider disableTransitionOnChange>
+      <CssBaseline />
+      <Box sx={{ display: 'flex', minHeight: '100dvh' }}>
+        <Sidebar />
+        <Header />
+        <Box component="main" className="MainContent" sx={{ flex: 1 }}>
+          <MyMessages />
+        </Box>
+      </Box>
+    </CssVarsProvider>
+  );
+}
