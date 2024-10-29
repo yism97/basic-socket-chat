50781f2345c09cc4cbb4ef2b57a611c1814d06a7
이상민 <yihs97@naver.com>
2024-10-29 15:25:35 +0900

    Feat 채팅방 입장기능 ,메시지 전송, 채팅방 나가기 기능 추가

--- server/index.js
+++ server/index.js
@@ -26,58 +26,65 @@ const io = new Server(httpServer, {
 });
 
 io.on("connection", (socket) => {
-  // 새로운 사용자가 입장했다는 메시지를 모든 사용자에게 공지
-  socket.broadcast.emit("SEND_MESSAGE", {
-    id: socket.id,
-    content: "새로운 사용자가 채팅방에 입장하였습니다.",
-    sender: socket.id,
-    timestamp: new Date().toLocaleString(),
-  });
   console.log("사용자가 연결되었습니다", socket.id);
 
-  socket.on("SEND_MESSAGE", async (msg) => {
-    const message = {
-      senderId: socket.id,
-      content: msg,
-      timestamp: new Date().toISOString(),
-    };
+  socket.on("JOIN_ROOM", (room) => {
+    if (!room) {
+      console.error("room이 존재하지 않습니다.");
+      return;
+    }
+    socket.join(room);
+    console.log(`새로운 사용자가 ${room} 방에 입장했습니다.`);
 
-    const Message = await prisma.message.create({
-      data: message,
-    });
-
-    console.log(message);
-
-    io.emit("SEND_MESSAGE", {
+    // 새로운 사용자가 입장했다는 메시지를 모든 사용자에게 공지
+    io.to(room).emit("SEND_MESSAGE", {
       id: socket.id,
-      content: Message.content,
-      sender: Message.senderId,
+      content: `새로운 사용자가 ${room} 방에 입장하였습니다.`,
+      sender: socket.id,
       timestamp: new Date().toLocaleString(),
     });
   });
 
-  socket.on("disconnect", () => {
-    socket.broadcast.emit("SEND_MESSAGE", {
-      // id: socket.id,
-      // content: "사용자가 채팅방을 나갔습니다",
-      // sender: socket.id,
-      // timestamp: new Date().toLocaleString(),
+  socket.on("SEND_MESSAGE", ({ room, message }) => {
+    if (!room || !message) {
+      console.error("room 또는 message 가 존재하지 않습니다.");
+      return;
+    }
+
+    io.to(room).emit("SEND_MESSAGE", {
+      id: socket.id + Math.random(),
+      content: message,
+      sender: socket.id,
+      timestamp: new Date().toLocaleString(),
     });
+    console.log(socket.id, "님의 SEND_MESSAGE", room, message);
+  });
+
+  socket.on("LEAVE_ROOM", (room) => {
+    if (room) {
+      // 본인한테도 자기가방을 나갔다는 메시지 띄우기
+      socket.emit("SEND_MESSAGE", {
+        id: socket.id,
+        content: `당신은 ${room} 방을 나갔습니다.`,
+        sender: socket.id,
+        timestamp: new Date().toLocaleString(),
+      });
+      socket.leave(room);
+      console.log(`${socket.id}님이 ${room} 방을 떠났습니다.`);
+
+      // 사용자가 방을 나갔다면 메시지를 모든 사용자에게 공지
+      io.to(room).emit("SEND_MESSAGE", {
+        id: socket.id,
+        content: `사용자 ${socket.id}님이 ${room} 방을 떠났습니다.`,
+        sender: socket.id,
+        timestamp: new Date().toLocaleString(),
+      });
+    }
+  });
+
+  socket.on("disconnect", () => {
     console.log("사용자가 연결을 끊었습니다", socket.id);
   });
-});
-app.get("/messages", async (req, res) => {
-  try {
-    const messages = await prisma.message.findMany({
-      orderBy: {
-        timestamp: "asc", // 시간순으로 오름차순하기(굳이 안해도 되긴할듯?)
-      },
-    });
-    return res.status(200).json(messages);
-  } catch (error) {
-    console.error("메시지를 조회 중 오류가 발생하였습니다.", error);
-    res.status(500).json({ error: "메시지를 조회 중 오류가 발생했습니다." });
-  }
 });
 
 app.use(express.static(path.join(path.resolve(), "public")));
